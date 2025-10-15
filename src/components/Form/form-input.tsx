b64a6b4b4814ddf30a00bddd083d05101482900b
Parvez <parvezz13913@gmail.com>
2025-10-15 08:26:25 +0600

    [DONE] Reusable Form component created

---
+++ src/components/Form/form-input.tsx
@@ -0,0 +1,13 @@
+interface IInput {
+  name: string;
+  type?: string;
+  value?: string | string[] | undefined;
+  id?: string;
+  placeholder?: string;
+  validation?: object;
+  label?: string;
+  required?: boolean;
+}
+const FormInput = () => {};
+
+export default FormInput;
